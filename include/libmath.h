fa9bc8dc60acd4a4f6dc99350d8770458c754e54
avkultra <avk_job@mail.ru>
2026-04-16 21:30:24 +0300

    math lib

---
+++ include/libmath.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace libmath
+{
+    int addition(int a, int b);
+    int subtraction(int a, int b);
+    int multiplication(int a, int b);
+    int division(int a, int b, int& c);
+    int power(const int& a, const int& pow);
+}
---
+++ src/libmath.cpp
@@ -0,0 +1,42 @@
+#include "libmath.h"
+
+namespace libmath
+{
+    int S_OK = 0;
+    int S_FALSE = -1;
+
+    int addition(const int a, int b)
+    {
+	return a + b;
+    }
+
+    int subtraction(int a, int b)
+    {
+	return a - b;
+    }
+
+    int multiplication(int a, int b)
+    {
+	return a * b;
+    }
+
+    int division(int a, int b, int& c)
+    {
+	if (!b)
+	    return S_FALSE;
+
+	c = a / b;
+	    return S_OK;
+    }
+
+    int power(const int& a, const int& pow)
+    {
+	int res = 1;
+	for (int i = 0; i < pow; ++i)
+	{
+	    res = res * a;
+	}
+	return res;
+    }
+
+}
